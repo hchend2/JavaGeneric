e3998bb0fc14f5d6df5d77f180ca3ebaf398cd11
herve chendjou <54636283+hchend2@users.noreply.github.com>
2022-02-03 02:08:11 +0000

    Initial commit

---
+++ GenericPractice.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Collections;
+
+class SomeGeneric<L extends Number & Comparable> {
+
+    private LinkedList<L> lList;
+    private L initVal;
+
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Collections;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
