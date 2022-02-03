8b572bbf60b73e3165e796695af885c3871be191
herve chendjou <54636283+hchend2@users.noreply.github.com>
2022-02-03 02:57:54 +0000

    add values

--- GenericPractice.java
+++ GenericPractice.java
@@ -5,15 +5,32 @@ import java.util.Collections;
 
 class SomeGeneric<L extends Number & Comparable> {
 
-    private LinkedList<L> lList;
+    private LinkedList<L> myList;
     private L initVal;
 
     //constructor...
-
     SomeGeneric(L val) {
         initVal = val;
-        lList = new LinkedList<>();
-        lList.add(val);
+        myList = new LinkedList<>();
+        myList.add(val);
+    }
+    // add item into linkedlist...
+    public void addToList(L val) {
+        myList.add(val);
+    }
+
+    // print list...
+    public void printList() {
+        if (myList.isEmpty()) {
+            System.out.println("List isEmpty");
+        }
+        Iterator<L> iter = myList.iterator();
+        while(true) {
+            if (!iter.hasNext()) {
+                break;
+            }
+            System.out.print(iter.next() + " ");
+        }
     }
 
 }
--- Main.java
+++ Main.java
@@ -5,6 +5,15 @@ import java.util.Collections;
 
 class Main {
   public static void main(String[] args) {
+      SomeGeneric<Integer> myVal = new SomeGeneric<>(10);
+      
+      myVal.addToList(1);
+      myVal.addToList(-1);
+      
+      System.out.println();
+      myVal.printList();
+      System.out.println();
+
     System.out.println("Hello world!");
   }
 }
